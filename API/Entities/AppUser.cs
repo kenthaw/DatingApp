65eb097b0c9b4a6276df495f8dafb3c2afcc5d89
kenthaw <kenthaw94@gmail.com>
2021-04-07 11:51:24 +0800

    end of section 14

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -22,9 +22,7 @@ namespace API.Entities
         public string Country { get; set; }
         public ICollection<Photos> Photos { get; set; }
 
-    //     public int GetAge()
-    //     {
-    //         return DateOfBirth.CalculateAge();
-    //     }
+        public ICollection<UserLike> LikedByUsers { get; set; }
+        public ICollection<UserLike> LikedUsers { get; set; }
     }
 }
